757ac5b4c630ac6f070afe90902ea3996af621f8
Fred Buchanan <oscillator.b@gmail.com>
2019-10-15 13:32:38 -0400

    Added check button

--- hexxed.js
+++ hexxed.js
@@ -5,15 +5,22 @@ var source  = "<p>Hexxed</p>"+
     "   <div class='color-slider'></div>" +
     "   <div class='color-slider'></div>" +
     "   <div class='color-slider'></div>" +
+    "   <button id='check' class=\"ui-button ui-widget ui-corner-all\">Check!</button>" +
     "</div>";
 
 (function($) {
+    var check = function() {
+        console.log('check')
+    }
+
     var scaffold = function(element) {
         $(element).html(source);
         $(".color-slider").slider({
             max: 255,
         });
+        $('#check').click(check);
     }
+
 
     $.fn.hexxed = function(settings) {
         console.log("New hexxed", settings);
