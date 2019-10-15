8fe586cedd54e58c3aabfac67856255be2e2f39d
Fred Buchanan <oscillator.b@gmail.com>
2019-10-15 13:39:50 -0400

    Added slider value getting

--- hexxed.js
+++ hexxed.js
@@ -2,16 +2,26 @@
 var source  = "<p>Hexxed</p>"+
     "<div id='color-square'></div>" +
     "<div>" +
-    "   <div class='color-slider'></div>" +
-    "   <div class='color-slider'></div>" +
-    "   <div class='color-slider'></div>" +
+    "   <div id='r' class='color-slider'></div>" +
+    "   <div id='g' class='color-slider'></div>" +
+    "   <div id='b' class='color-slider'></div>" +
     "   <button id='check' class=\"ui-button ui-widget ui-corner-all\">Check!</button>" +
     "</div>";
 
 (function($) {
-    var check = function() {
-        console.log('check')
+
+    var getColorInSliders = function() {
+        return {
+            'r': $('#r').slider("option","value"),
+            'g': $('#g').slider("option","value"),
+            'b': $('#b').slider("option","value")
+        }
     }
+
+    var check = function() {
+        var color = getColorInSliders();
+        console.log(color);
+    };
 
     var scaffold = function(element) {
         $(element).html(source);
@@ -19,7 +29,7 @@ var source  = "<p>Hexxed</p>"+
             max: 255,
         });
         $('#check').click(check);
-    }
+    };
 
 
     $.fn.hexxed = function(settings) {
